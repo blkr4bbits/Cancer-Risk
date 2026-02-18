da0042f2625092a77b3bcd67641fde150db43f6f
jgreen2023 <134550521+jgreen2023@users.noreply.github.com>
2026-02-18 17:01:40 -0500

    added documentation and comments to testing.py

--- testing.py
+++ testing.py
@@ -5,7 +5,8 @@ from torchvision import datasets
 from torchvision.transforms import ToTensor
 import pandas as pd
 import matplotlib.pyplot as plt
-
+import time
+from statsmodels.miscmodels.ordinal_model import OrderedModel
 
 
 
@@ -24,14 +25,35 @@ print("""
 leukemia_data = "Leukemia_GSE9476.csv"
 df = pd.read_csv(leukemia_data)
 
-data = df.head()
-print(f"Overall data: \n \n")
-print(data)
+# LOAD AND READ DATA WITH PD FROM PANDA
+
+data = df.head(5) 
+
+# creates the rows to establish the data frame 
+
+print(f"Overall data: \n \n") 
+print(data) 
+
+#prints the data and makes it organized
 
 if df.columns[0].lower() in ["id", "sample", "unnamed: 0"]:
     df = df.iloc[:, 1:]
 
-matrix_of_data = df.iloc[:3, :6] # [:y :x]
+matrix_of_data = df.iloc[:3, :6] 
+
+'''
+[:y :x] creates the tensor 
+matrix of data by slicing the data frame, 
+the first 3 rows and the first 6 columns
+'''
 
 print(f"Matrix subset of Dataset: \n \n")
 print(matrix_of_data)
+
+
+
+
+data['sample1'].dtypes
+
+
+mod_ord = OrderedModel()
