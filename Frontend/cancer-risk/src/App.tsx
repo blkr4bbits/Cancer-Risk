37f3e216fd213b00f6c9ab37b6ed194aadf21917
Josh <joshua.e.green2023@gmail.com>
2026-05-21 12:18:02 -0400

    quick file transfer from laptop

--- Frontend/cancer-risk/src/App.tsx
+++ Frontend/cancer-risk/src/App.tsx
@@ -4,7 +4,13 @@ function App () {
     <div> 
       
       <h1> CRAM - Cancer Risk Assessment Model </h1> 
-      
+      <hr></hr>
+      <p> Welcome to the Cancer Risk Assesment Model, this is a tool 
+        designed to help medical professionals identify cancer risk in 
+        patients with a simple csv dataset </p>
+      <hr></hr>
+      <p id = "instructions"> To use this tool, upload a csv file and wait for the results to be displayed </p>
+      <hr></hr>
     </div>
   )
 }
